393f72d8dfc2487a6f23eca9d9d1d447243286e6
carlos recinos portillo <Alexrecinos@iMac-de-carlos.local>
2020-02-27 09:56:03 -0800

    Initial Commit

--- dist/app.js
+++ dist/app.js
@@ -1,7 +1,7 @@
 const navSlide = () => {
   const burger = document.querySelector(".burger");
   const nav = document.querySelector(".nav-list");
-  const navLinks = document.querySelectorAll(".nav-item");
+  const navLinks = document.querySelectorAll(".list-item");
 
   //Toggle nav
   burger.addEventListener("click", () => {
@@ -21,3 +21,51 @@ const navSlide = () => {
   });
 };
 navSlide();
+
+// animation
+AOS.init({
+  offset: 250,
+  duration: 1000
+});
+
+// smooth scroll
+
+function smoothScroll(target, duration) {
+  let target1 = document.querySelector(target);
+
+  // get the position
+  let targetPosition = target1.getBoundingClientRect().top;
+
+  let startPosition = window.pageYOffset;
+
+  // get the distance
+  let distance = targetPosition - startPosition;
+  var startTime = null;
+
+  function animation(currentTime) {
+    if (startTime === null) startTime = currentTime;
+    let timeElapsed = currentTime - startTime;
+    let run = ease(timeElapsed, startPosition, distance, duration);
+
+    // scroll verticaly
+    window.scrollTo(0, run);
+
+    if (timeElapsed < duration) requestAnimationFrame(animation);
+  }
+
+  //  ease animation function parameters (timeElapsed, startPosition, distance, duration)
+  function ease(t, b, c, d) {
+    t /= d / 2;
+    if (t < 1) return (c / 2) * t * t + b;
+    t--;
+    return (-c / 2) * (t * (t - 2) - 1) + b;
+  }
+
+  requestAnimationFrame(animation);
+}
+
+let services = document.querySelector(".services");
+
+services.addEventListener("click", () => {
+  smoothScroll("#section2", 1500);
+});
